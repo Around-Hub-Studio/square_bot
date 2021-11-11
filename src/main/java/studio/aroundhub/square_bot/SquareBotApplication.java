4e0b1ff574e6adf96a452e3a2d87e8e441d19063
Flature <jungwoo5394@gmail.com>
2021-11-11 17:06:08 +0900

    Initial commit

---
+++ src/main/java/studio/aroundhub/square_bot/SquareBotApplication.java
@@ -0,0 +1,13 @@
+package studio.aroundhub.square_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquareBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/studio/aroundhub/square_bot/SquareBotApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.aroundhub.square_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
